d00811507162a1cf5b502315d368bc88e86b8177
hao <tanhao@seas.upenn.edu>
2023-11-02 03:55:37 -0400

    Pomodoro timer project, 11/02/2023.

---
+++ main.py
@@ -0,0 +1,96 @@
+from tkinter import *
+import math
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
+
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    # if it is the 8th rep:
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    # if it is the 2st/4rd/6th rep:
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=RED)
+    else:
+        # if it is the 1st/3rd/5th/7th rep:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)  # after 1000ms(1s), call the function count_down and pass in (count-1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title = ("pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# widgets
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+title_label.grid(column=1, row=0)
+
+# canvas
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato = PhotoImage(file="tomato.png")  # or a path if not in the same folder
+canvas.create_image(100, 112, image=tomato)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+window.mainloop()
